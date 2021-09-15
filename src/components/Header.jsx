c0bb335f76e581278010d641bc665a2521bb2410
Paulo lacerda <xcloudgameemail@gmail.com>
2021-09-15 15:02:37 -0300

    Resolve o requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,9 @@ class Header extends React.Component {
         <h3 data-testid="header-user-name" value={ name }>
           {`${name}`}
         </h3>
+        <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
