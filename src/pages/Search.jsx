118e5c3c0a952a28b4c080c58c562ec7e960d364
Paulo lacerda <xcloudgameemail@gmail.com>
2021-09-15 15:14:51 -0300

    Requisito 5 resolvido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
+    const { artistName } = this.state;
+    const NUMBER_LENGTH = 2;
+    const enabled = artistName.length >= NUMBER_LENGTH;
     return (
       <div data-testid="page-search">
-        <p>Search</p>
+        <form>
+          <input
+            type="text"
+            name="artistName"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ !enabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
