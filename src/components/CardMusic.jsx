319dc3f31ca1b16cfe4f9d573c7c474e842a66a6
Paulo lacerda <xcloudgameemail@gmail.com>
2021-09-16 08:33:50 -0300

    Requisito 6 concluido

---
+++ src/components/CardMusic.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class CardMusic extends React.Component {
+  render() {
+    const { collectionId, artworkUrl100, collectionName, artistName } = this.props;
+    return (
+      <div>
+        <img src={ artworkUrl100 } alt={ collectionName } />
+        <Link
+          to={ `/album/${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        >
+          <h3>{ collectionName }</h3>
+        </Link>
+        <h4>{ artistName }</h4>
+
+      </div>
+    );
+  }
+}
+
+export default CardMusic;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,24 @@
+/* eslint-disable max-len */
 import React from 'react';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
+import CardMusic from '../components/CardMusic';
 
 class Search extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      artistName: '',
+      artist: '',
+      albums: '',
+      loading: false,
+      resultEnabled: false,
+      pesquisaAtual: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.searchAlbums = this.searchAlbums.bind(this);
+    this.functionTest = this.functionTest.bind(this);
   }
 
   handleChange(event) {
@@ -17,27 +27,75 @@ class Search extends React.Component {
     });
   }
 
+  // albums.map((album) => (album.artistName.includes(artist) ? album.artistName : null));
+  async searchAlbums() {
+    const { artist } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await searchAlbumsAPI(artist)
+      .then(
+        (result) => {
+          this.setState({
+            albums: result,
+            loading: false,
+            pesquisaAtual: artist,
+            resultEnabled: true,
+            artist: '',
+          });
+        },
+      );
+  }
+
+  functionTest() {
+    const { albums } = this.state;
+    console.log(albums);
+  }
+
   render() {
-    const { artistName } = this.state;
+    const { artist, albums, loading, pesquisaAtual, resultEnabled } = this.state;
     const NUMBER_LENGTH = 2;
-    const enabled = artistName.length >= NUMBER_LENGTH;
+    const enabled = artist.length >= NUMBER_LENGTH;
+
+    // if (resultEnabled) return (<p> Resultado de álbuns de: </p>);
+    if (loading) return <Loading />;
+    let resEnabled = '';
+    let vazioEnabled = '';
+    if (resultEnabled) resEnabled = <p>{`Resultado de álbuns de: ${pesquisaAtual}`}</p>;
+    if (albums.length === 0 && resultEnabled) vazioEnabled = <p>Nenhum álbum foi encontrado</p>;
+
     return (
       <div data-testid="page-search">
         <form>
           <input
             type="text"
-            name="artistName"
+            name="artist"
             data-testid="search-artist-input"
             onChange={ this.handleChange }
           />
           <button
             type="button"
             data-testid="search-artist-button"
+            onClick={ this.searchAlbums }
             disabled={ !enabled }
           >
             Pesquisar
           </button>
         </form>
+
+        <section>
+          { resEnabled }
+          <div>
+            { vazioEnabled }
+            { albums && albums.map((album) => (<CardMusic
+              key={ album.collectionId }
+              artistName={ album.artistName }
+              collectionName={ album.collectionName }
+              collectionId={ album.collectionId }
+              artworkUrl100={ album.artworkUrl100 }
+            />))}
+          </div>
+        </section>
       </div>
     );
   }
